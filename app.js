10dcc49680d7a0180408b8c18f6410c8d4354e3b
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-04-28 18:02:02 +0300

    adding a function that gets the choice of our computer

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 // Create an array containing all the available choices:
 const choiceArray = ['Rock', 'Paper', 'Scissors'];
 
+// A function that gets the choice of our computer:
+const getComputerChoice = (choices) => {
+    return (choices[Math.floor(Math.random() * choices.length)]);
+}
+// console.log(getComputerChoice(choiceArray));
