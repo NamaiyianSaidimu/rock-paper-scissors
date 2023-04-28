f7b86496a0acdde38f095b47b9f4cd68e8b6fe3d
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-04-29 00:58:51 +0300

    adding a function to play a single round of the game

--- app.js
+++ app.js
@@ -6,3 +6,30 @@ const getComputerChoice = (choices) => {
     return (choices[Math.floor(Math.random() * choices.length)]);
 }
 // console.log(getComputerChoice(choiceArray));
+
+let oneRound = (playerSelection, computerSelection = getComputerChoice(choiceArray).toLowerCase()) => {
+    //computerSelection = getComputerChoice(choiceArray).toLowerCase();
+    let playerChoice = playerSelection.toLowerCase();
+    if (playerChoice == computerSelection) {
+        return ("Tie!")
+    }
+    else if (playerChoice == 'rock' && computerSelection == 'scissors') {
+        return ("You win! Rock beats Scissors.");
+    }
+    else if (playerChoice == 'paper' && computerSelection == 'rock') {
+        return ("You win! Paper covers Rock.");
+    }
+    else if (playerChoice == 'scissors' && computerSelection == 'paper') {
+        return ("You win! Scissors cuts paper.");
+    }
+    else if (playerChoice == 'scissors' && computerSelection == 'rock') {
+        return ("You lose! Rock beats Scissors.");
+    }
+    else if (playerChoice == 'rock' && computerSelection == 'paper') {
+        return ("You lose! Paper covers Rock.");
+    }
+    else if (playerChoice == 'paper' && computerSelection == 'scissors') {
+        return ("You lose! Scissors cuts Paper.");
+    }
+}
+// console.log(oneRound('scissORS'));
